4120abe456a42c45290fdbfccdfeed95932cf350
Sarath Menon <sarathmenon@mailbox.org>
2023-06-19 18:04:31 +0200

    update docs tree

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,9 +38,9 @@ copyright = u'2019, Sarath Menon'
 author = u'Sarath Menon'
 
 # The short X.Y version
-version = u'3.0.0.dev.6'
+version = u'3.0.0'
 # The full version, including alpha/beta/rc tags
-release = u'3.0.0.dev.6'
+release = u'3.0.0'
 
 
 # -- General configuration ---------------------------------------------------
