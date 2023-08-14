1b0a200483ad13f0e2f90a48666c74ce223d51a2
Sarath Menon <sarathmenon@mailbox.org>
2023-08-14 11:20:01 +0200

    test fractional repetition

--- src/pyscal3/operations/operations.py
+++ src/pyscal3/operations/operations.py
@@ -105,6 +105,125 @@ def repeat(system, repetitions, ghost = False,
         return system
 
 
+def repeat_fractional(system, repetitions, ghost = False,
+    scale_box = True, atoms = None, return_atoms = False, return_box=False):
+    """
+    Repeat the given system
+
+    Parameters
+    ----------
+    system: pyscal System object
+        the input system to be repeated
+
+    repetitions: tuple of double
+        number of times the system is to be rotated in each direction
+
+    ghost: bool, optional
+        if True, make the new atoms ghost, default False
+
+    scale_box: bool, optional
+        if True, scale the simulation box, default True
+
+    atoms: None, optional
+        if provided use the given atoms, and not the atoms from the system
+
+    return_atoms: bool, optional
+        if True, return atoms instead of adding them to the system. Default False
+    
+    Returns
+    -------
+    system: pyscal System object
+        the system with repetitions. Only returned if `return_atoms` is False.
+    
+    atoms: Atoms object
+        only returned if `return_atoms` is True.
+
+
+    """
+    box = system.box        
+    system.actual_box = box.copy()
+
+    if atoms is None:
+        atoms = system.atoms
+
+    newatoms = []
+    idstart = len(atoms) + 1
+
+    x_lo = repetitions[0]*self.boxdims[0]
+    x_hi = (repetitions[0]+1)*self.boxdims[0]
+    y_lo = repetitions[1]*self.boxdims[1]
+    y_hi = (repetitions[1]+1)*self.boxdims[1]
+    z_lo = repetitions[2]*self.boxdims[2]
+    z_hi = (repetitions[2]+1)*self.boxdims[2]
+    xs = 2*repetitions[0] + 1
+    ys = 2*repetitions[1] + 1
+    zs = 2*repetitions[2] + 1
+    
+    datadict = {key:[] for key in atoms.keys()}
+    del datadict['positions']
+    del datadict['ids']
+    del datadict['head']
+    del datadict['ghost']
+    positions = []
+    ids = []
+    head = []
+    ghosts = []
+
+    #first loop directly over atoms
+    for count, pos in enumerate(atoms['positions']):
+        if not atoms["ghost"][count]:
+            #now check coordinates one by one
+            mod = False
+            if (pos[0] <= x_lo):
+                pos = pos + np.array(box[0])
+                mod = True
+            elif (pos[0] > x_hi):
+                pos = pos - np.array(box[0])
+                mod = True  
+            if (pos[1] <= y_lo):
+                pos = pos + np.array(box[1])
+                mod = True
+            elif (pos[1] > y_hi):
+                pos = pos - np.array(box[1])
+                mod = True  
+            if (pos[2] <= z_lo):
+                pos = pos + np.array(box[2])
+                mod = True
+            elif (pos[2] > z_hi):
+                pos = pos - np.array(box[2])
+                mod = True
+            if mod:  
+                positions.append(list(pos))
+                ids.append(idstart)
+                head.append(count)
+                ghosts.append(ghost)
+                idstart += 1
+                for key in datadict.keys():
+                    datadict[key].append(atoms[key][count])
+
+    if scale_box:
+        box[0] = xs*np.array(box[0])
+        box[1] = ys*np.array(box[1])
+        box[2] = zs*np.array(box[2])
+    if ghost:
+        system.ghosts_created = True
+
+    atoms['positions'].extend(positions)
+    atoms['ids'].extend(ids)
+    atoms['ghost'].extend(ghosts)
+    atoms['head'].extend(head)
+    for key in datadict.keys():
+        atoms[key].extend(datadict[key])
+
+    if return_atoms:
+        return atoms, box
+
+    else:
+        system.box = box
+        system.atoms = atoms
+        return system
+
+
 def embed_in_cubic_box(system, input_box=None, 
     return_box=False):
     """
