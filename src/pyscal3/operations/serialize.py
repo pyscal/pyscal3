ba53253ed24574dc4f6cad3a6a7f994c5e7f6827
Sarath Menon <sarathmenon@mailbox.org>
2023-11-29 12:07:00 +0100

    bug fix in serialise

--- src/pyscal3/operations/serialize.py
+++ src/pyscal3/operations/serialize.py
@@ -40,21 +40,22 @@ def serialize(system, return_type='dict', outputfile=None):
 
 	if system._structure_dict is not None:
 		sample.material.crystal_structure.altname = system.atoms._lattice
-		sample.material.crystal_structure.spacegroup_symbol = prp.get_space_group(system)
-		sample.material.crystal_structure.spacegroup_number = prp.get_space_group(system)
+		spg = prp.get_space_group(system)
+		sample.material.crystal_structure.spacegroup_symbol = spg[0]
+		sample.material.crystal_structure.spacegroup_number = spg[1]
 		sample.material.crystal_structure.unit_cell.bravais_lattice = prp.get_bravais_lattice(system)
 		sample.material.crystal_structure.unit_cell.lattice_parameter = system.atoms._lattice_constant
-		sample.material.crystal_structure.unit_cell.angle = [prp.get_angle(system.box[0], system.box[1]),
+		sample.material.crystal_structure.unit_cell.angle = list([prp.get_angle(system.box[0], system.box[1]),
 															prp.get_angle(system.box[1], system.box[2]),
-															prp.get_angle(system.box[2], system.box[0])]
+															prp.get_angle(system.box[2], system.box[0])])
 
 	sample.simulation_cell.volume = system.volume
 	sample.simulation_cell.number_of_atoms = system.natoms
-	sample.simulation_cell.length = system.box_dimensions
-	sample.simulation_cell.vector = system.box
-	sample.simulation_cell.angle = [prp.get_angle(system.box[0], system.box[1]),
+	sample.simulation_cell.length = list(system.box_dimensions)
+	sample.simulation_cell.vector = [list(x) for x in system.box]
+	sample.simulation_cell.angle = list([prp.get_angle(system.box[0], system.box[1]),
 															prp.get_angle(system.box[1], system.box[2]),
-															prp.get_angle(system.box[2], system.box[0])]
+															prp.get_angle(system.box[2], system.box[0])])
 
 	if return_type=='dict':
 		return sample.dict()
