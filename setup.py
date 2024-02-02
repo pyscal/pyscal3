488d7d00c165f261cbf065826aa866a0b951f884
Sarath Menon <sarathmenon@mailbox.org>
2024-02-02 16:19:36 +0100

    Bump version: 3.1.5 → 3.1.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md') as readme_file:
 
 setup(
     name='pyscal3',
-    version='3.1.5',
+    version='3.1.6',
     author='Sarath Menon',
     author_email='sarath.menon@pyscal.org',
     description='Python library written in C++ for calculation of local atomic structural environment',
