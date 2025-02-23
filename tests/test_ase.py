06e7211c857ff6e6646ee22a783a37148b3024a9
Jan Janßen <janssen@mpie.de>
2025-02-23 12:55:30 +0100

    Update user interface - import pyscal3.ase as psa

--- tests/test_ase.py
+++ tests/test_ase.py
@@ -2,19 +2,13 @@ import pytest
 import os,sys,inspect
 import numpy as np
 import pyscal3.core as pc
-from pyscal3.ase import (
-    calculate_centrosymmetry,
-    calculate_cna,
-    calculate_diamond_structure,
-    calculate_radial_distribution_function,
-    calculate_steinhardt_parameter,
-)
+import pyscal3.ase as psa
 from ase.build import bulk
 
 
 def test_calculate_centrosymmetry():
     ti_hcp = bulk("Ti", orthorhombic=True)
-    q = calculate_centrosymmetry(ti_hcp, nmax=12)
+    q = psa.calculate_centrosymmetry(ti_hcp, nmax=12)
     assert np.round(np.mean(np.array(q)), decimals=2) == 8.7
 
 
@@ -23,19 +17,19 @@ def test_calculate_cna():
     fe_bcc = bulk("Fe")
     ti_hcp = bulk("Ti")
 
-    cna = calculate_cna(al_fcc)
+    cna = psa.calculate_cna(al_fcc)
     assert cna["fcc"] == 1
 
-    cna = calculate_cna(fe_bcc)
+    cna = psa.calculate_cna(fe_bcc)
     assert cna["bcc"] == 1
 
-    cna = calculate_cna(ti_hcp)
+    cna = psa.calculate_cna(ti_hcp)
     assert cna["hcp"] == 2
 
 
 def test_calculate_steinhardt_parameter():
     w_bcc = bulk("W", a=1.0, cubic=True).repeat([4, 4, 4])
-    q = calculate_steinhardt_parameter(
+    q = psa.calculate_steinhardt_parameter(
         ase_atoms=w_bcc,
         q=[4, 6],
         nmax=12,
@@ -46,7 +40,7 @@ def test_calculate_steinhardt_parameter():
     assert np.round(np.mean(np.array(q[1])), decimals=2) == 0.63, "Calculated q4 value is wrong!"
 
     w_bcc = bulk("W", a=1.0, cubic=True).repeat([4, 4, 4])
-    q = calculate_steinhardt_parameter(
+    q = psa.calculate_steinhardt_parameter(
         ase_atoms=w_bcc,
         q=[4, 6],
         nmax=12,
@@ -65,7 +59,7 @@ def test_calculate_diamond_structure():
         repetitions=(2,2,2),
         lattice_constant=4.00,
     ).convert_to.ase()
-    diamond_dict = calculate_diamond_structure(c_diamond)
+    diamond_dict = psa.calculate_diamond_structure(c_diamond)
     assert diamond_dict['cubic diamond'] == len(c_diamond)
 
 
@@ -73,10 +67,10 @@ def test_calculate_radial_distribution_function():
     w_bcc = bulk("W", a=1.00).repeat([10, 10, 10])
     al_fcc = bulk("Al", a=1.00).repeat([10, 10, 10])
 
-    rdf, r = calculate_radial_distribution_function(w_bcc, rmax=2)
+    rdf, r = psa.calculate_radial_distribution_function(w_bcc, rmax=2)
     args = np.argsort(rdf)[::-1]
     assert(r[args[0]] - 0.86 < 1E-5)
 
-    rdf, r = calculate_radial_distribution_function(al_fcc, rmax=2)
+    rdf, r = psa.calculate_radial_distribution_function(al_fcc, rmax=2)
     args = np.argsort(rdf)[::-1]
     assert(r[args[0]] - 0.70 < 1E-5)
