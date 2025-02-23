09ca4e489a50574e49efd465527d31c964457b04
Jan Janßen <janssen@mpie.de>
2025-02-23 11:46:54 +0100

    Implement calculate_steinhardt_parameter()

--- src/pyscal3/ase.py
+++ src/pyscal3/ase.py
@@ -1,45 +1,62 @@
 import inspect
 
-
 from ase.atoms import Atoms
 
 import pyscal3.core as pc
 from pyscal3.operations.centrosymmetry import calculate_centrosymmetry as _calculate_centrosymmetry
+from pyscal3.operations.calculations import calculate_q as _calculate_q
 from pyscal3.operations.cna import calculate_cna as _calculate_cna
+from pyscal3.operations.identify import find_neighbors
 
 
 def _get_structure(ase_atoms: Atoms) -> pc.System:
     return pc.System(ase_atoms, format='ase')
 
 
-def _get_updated_signature(signature: inspect.Signature) -> inspect.Signature:
+def _get_updated_signature(signature: inspect.Signature, add_find_neighbors: bool = False) -> inspect.Signature:
     parameters = signature.parameters.copy()
     del parameters["system"]
-    ase_atoms_parameter = inspect.Parameter(
+    parameters["ase_atoms"] = inspect.Parameter(
         name="ase_atoms",
         kind=inspect.Parameter.POSITIONAL_OR_KEYWORD,
         annotation=Atoms,
     )
-    parameters["ase_atoms"] = ase_atoms_parameter
+    if add_find_neighbors:
+        for k, v in inspect.signature(find_neighbors).parameters.items():
+            if k != "system":
+                parameters[k] = v
     parameters.move_to_end("ase_atoms", last = False)
     return signature.replace(parameters=parameters.values())
 
 
-def _wrap_function(input_function: callable) -> callable:
-    sig_updated = _get_updated_signature(signature=inspect.signature(input_function))
+def _wrap_function(funct: callable, add_find_neighbors: bool = False) -> callable:
+    sig_updated = _get_updated_signature(
+        signature=inspect.signature(funct),
+        add_find_neighbors=add_find_neighbors,
+    )
 
     def ase_compatibility_wrapper(*args, **kwargs):
         input_dict = sig_updated.bind(*args, **kwargs).arguments
-        input_dict["system"] = _get_structure(input_dict.pop("ase_atoms"))
-        return input_function(**input_dict)
+        pc_system = _get_structure(input_dict.pop("ase_atoms"))
+        if add_find_neighbors:
+            find_neighbor_keys = inspect.signature(find_neighbors).parameters.keys()
+            find_neighbors_kwargs = {
+                k: input_dict.pop(k)
+                for k in find_neighbor_keys
+                if k != "system" and k in input_dict
+            }
+            pc_system.find.neighbors(**find_neighbors_kwargs)
+        input_dict["system"] = pc_system
+        return funct(**input_dict)
 
-    ase_compatibility_wrapper.__name__ = input_function.__name__
+    ase_compatibility_wrapper.__name__ = funct.__name__
     ase_compatibility_wrapper.__signature__ = sig_updated
-    ase_compatibility_wrapper.__doc__ = input_function.__doc__.replace(
+    ase_compatibility_wrapper.__doc__ = funct.__doc__.replace(
         "system: System object", "ase_atoms: ase.atoms.Atoms object"
     )
     return ase_compatibility_wrapper
 
 
-calculate_centrosymmetry = _wrap_function(_calculate_centrosymmetry)
-calculate_cna = _wrap_function(_calculate_cna)
+calculate_centrosymmetry = _wrap_function(funct=_calculate_centrosymmetry, add_find_neighbors=False)
+calculate_cna = _wrap_function(funct=_calculate_cna, add_find_neighbors=False)
+calculate_steinhardt_parameter = _wrap_function(funct=_calculate_q, add_find_neighbors=True)
--- tests/test_ase.py
+++ tests/test_ase.py
@@ -1,7 +1,7 @@
 import pytest
 import os,sys,inspect
 import numpy as np
-from pyscal3.ase import calculate_centrosymmetry, calculate_cna
+from pyscal3.ase import calculate_centrosymmetry, calculate_cna, calculate_steinhardt_parameter
 from ase.build import bulk
 
 
@@ -24,3 +24,28 @@ def test_calculate_cna():
 
     cna = calculate_cna(ti_hcp)
     assert cna["hcp"] == 2
+
+
+def test_calculate_steinhardt_parameter():
+    w_bcc = bulk("W", a=1.0, cubic=True).repeat([4, 4, 4])
+    q = calculate_steinhardt_parameter(
+        ase_atoms=w_bcc,
+        q=[4, 6],
+        nmax=12,
+        method='cutoff',
+        cutoff=0.9,
+    )
+    assert np.round(np.mean(np.array(q[0])), decimals=2) == 0.51, "Calculated q4 value is wrong!"
+    assert np.round(np.mean(np.array(q[1])), decimals=2) == 0.63, "Calculated q4 value is wrong!"
+
+    w_bcc = bulk("W", a=1.0, cubic=True).repeat([4, 4, 4])
+    q = calculate_steinhardt_parameter(
+        ase_atoms=w_bcc,
+        q=[4, 6],
+        nmax=12,
+        method='cutoff',
+        cutoff=0.9,
+        averaged=True,
+    )
+    assert np.round(np.mean(np.array(q[0])), decimals=2) == 0.51, "Calculated q4 value is wrong!"
+    assert np.round(np.mean(np.array(q[1])), decimals=2) == 0.63, "Calculated q4 value is wrong!"
